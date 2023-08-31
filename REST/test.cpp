e1b9437557a2d863fa8c075f4da32105d6d0878a
Felipe <cristian.castillo.barrero@deutsche-boerse.com>
2023-08-31 17:01:55 +0200

    Code refactoring

--- REST/test.cpp
+++ REST/test.cpp
@@ -1,36 +1,26 @@
 #include <iostream>
 #include <curl/curl.h>
 
-// Callback function to process received data.
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) 
-{
-    size_t totalSize = size * nmemb;
-    std::string *output = static_cast<std::string*>(userp);
-    output->append(static_cast<char*>(contents), totalSize);
-    return totalSize;
-}
-
-int main()
-{
-    // A pointer to a cURL handle. This is used to initialize and perform cURL requests.
-    CURL *curl;
-    // Store the return code of cURL requests.
-    CURLcode res;
-    // Store the response from the API.
-    std::string apiResponse;
-    // Initialize cURL.
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-
-    // Initialize a cURL handle.
-    curl = curl_easy_init();
-
-    if (!curl)
+// Callback function to process received data
+    size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *output)
     {
+        size_t totalSize = size * nmemb;
+        // Append received data to the output string
+        output->append(static_cast<char*>(contents), totalSize);
+        return totalSize;
+    }
+
+int main() {
+    // Initialize cURL
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    CURL *curl = curl_easy_init();
+    if (!curl) {
         // Handle cURL initialization failure
         std::cerr << "cURL initialization failed." << std::endl;
         return 1;
     }
 
+    std::string apiResponse;
     // Set the URL to request
     curl_easy_setopt(curl, CURLOPT_URL, "https://api.bitfinex.com/v1/trades/btcusd");
     // Set the callback function for writing received data
@@ -39,10 +29,9 @@ int main()
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &apiResponse);
 
     // Perform the cURL request
-    res = curl_easy_perform(curl);
-
-    if (res != CURLE_OK)
-    {
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+        // Handle cURL request failure
         std::cerr << "cURL request failed: " << curl_easy_strerror(res) << std::endl;
     }
 
@@ -52,9 +41,5 @@ int main()
 
     // Print the API response
     std::cout << apiResponse << std::endl;
-
     return 0;
 }
-
-// Build command
-// g++ main.cpp -o main -lcurl
