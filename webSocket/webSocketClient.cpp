e99317e729dda868d667ee3a9db5d606d363bd8f
Felipe <cristian.castillo.barrero@deutsche-boerse.com>
2023-09-01 11:00:23 +0200

    New code, created by Tim

--- webSocket/webSocketClient.cpp
+++ webSocket/webSocketClient.cpp
@@ -1,60 +1,97 @@
-#include <websocketpp/config/asio_client.hpp> // Use TLS
-// #include <websocketpp/config/asio_no_tls_client.hpp>   No TLS
 #include <websocketpp/client.hpp>
+#include <websocketpp/config/asio_client.hpp>
 
-#include <iostream>
-#include <string>
 
-using client = websocketpp::client<websocketpp::config::asio_tls_client>;  // Use TLS
-// using client = websocketpp::client<websocketpp::config::asio_client>;      No TLS
+using Client = websocketpp::client<websocketpp::config::asio_tls_client>;
 
-void on_message(client* c, websocketpp::connection_hdl hdl, client::message_ptr msg)
+using ConnectionHdl = websocketpp::connection_hdl;
+
+using SslContext = websocketpp::lib::asio::ssl::context;
+
+using websocketpp::lib::placeholders::_1;
+
+using websocketpp::lib::placeholders::_2;
+
+ 
+
+void on_message(Client* client, ConnectionHdl hdl, websocketpp::config::asio_client::message_type::ptr msg)
 {
-   std::cout << "Received: " << msg->get_payload() << std::endl;
+  std::cout << "on_message: " << msg->get_payload() << std::endl;
+  client->close(hdl, websocketpp::close::status::normal, "done");
 }
 
 
-int main() {
-
-   client c;
-
-   // Set logging to more silent than normal (optional)
-   c.set_access_channels(websocketpp::log::alevel::none);
-   // c.set_error_channels(websocketpp::log::elevel::none); Silence error output
-
-   // Initialize ASIO transport and set the handler for messages.
-   c.init_asio();
-   c.set_message_handler(std::bind(&on_message, &c, std::placeholders::_1, std::placeholders::_2));
-
-   // Create a connection to Bitfinex's WSS endpoint
-   websocketpp::lib::error_code ec;
-   client::connection_ptr con;
-std::cout << "Last working line" << std::endl;
-
-   con->set_tls_init_handler([](websocketpp::connection_hdl)
-   {
-      return websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::tlsv12_client);
-   });
-
-   c.get_connection("wss://api-pub.bitfinex.com/ws/2", ec);
-
-   if (ec)
-   {
-       std::cout << "Error creating connection: " << ec.message() << std::endl;
-       return 1;
-   }
-
-   // Connect
-   c.connect(con);
-
-   // Start the ASIO io_service run loop
-   c.run();
-
-   return 0;
+void on_open(Client* client, ConnectionHdl hdl)
+{
+  std::string msg{"hello"};
+  std::cout << "on_open: send " << msg << std::endl;
+  client->send(hdl, msg, websocketpp::frame::opcode::text);
 }
 
-// Old Build
-// g++ -std=c++2a webSocketClient.cpp -o webSocketClient -lpthread -lboost_system -lboost_thread -lssl -lcrypto -ldl
 
-// New Build
+websocketpp::lib::shared_ptr<SslContext> on_tls_init()
+{
+  auto ctx = websocketpp::lib::make_shared<SslContext>(boost::asio::ssl::context::sslv23);
+  return ctx;
+}
+ 
+
+void turn_off_logging(Client& client)
+{
+  client.clear_access_channels(websocketpp::log::alevel::all);
+  client.clear_error_channels(websocketpp::log::elevel::all);
+}
+
+
+void set_message_handler(Client& client)
+{
+  client.set_message_handler(websocketpp::lib::bind(&on_message, &client, ::_1, ::_2));
+}
+
+
+void set_open_handler(Client& client)
+{
+  client.set_open_handler(websocketpp::lib::bind(&on_open, &client, ::_1));
+}
+
+
+void set_tls_init_handler(Client& client)
+{
+  client.set_tls_init_handler(websocketpp::lib::bind(&on_tls_init));
+}
+
+
+void set_url(Client& client, std::string url)
+{
+  websocketpp::lib::error_code ec;
+
+  auto connection = client.get_connection(url, ec);
+
+  client.connect(connection);
+}
+
+ 
+
+int main()
+{
+  Client client;
+
+  // turn_off_logging(client);
+
+  client.init_asio();
+
+  set_tls_init_handler(client);
+
+  set_open_handler(client);
+
+  set_message_handler(client);
+
+  set_url(client, "wss://api-pub.bitfinex.com/ws/2:443");
+
+
+  client.run();
+
+  return 0;
+}
+
 // g++ webSocketClient.cpp -o webSocketClient -lpthread -lssl -lcrypto
