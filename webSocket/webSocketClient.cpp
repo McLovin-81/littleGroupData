bbd8ace627d55063c1f0d7b8da19b3e2e4b02dc0
Cristian Felipe <estilo-callejero@hotmail.de>
2023-09-03 21:00:30 +0200

    Comments for more understanding.

--- webSocket/webSocketClient.cpp
+++ webSocket/webSocketClient.cpp
@@ -12,37 +12,38 @@ using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 
  
-
+// Callback function when a message is received.
 void on_message(Client* client, ConnectionHdl hdl, websocketpp::config::asio_client::message_type::ptr msg)
 {
-  std::cout << "on_message: " << msg->get_payload() << std::endl;
-  client->close(hdl, websocketpp::close::status::normal, "done");
+  std::cout << "callback ↓: " << msg->get_payload() << std::endl;
+  // client->close(hdl, websocketpp::close::status::normal, "done");
 }
 
-
+// Send message.
 void on_open(Client* client, ConnectionHdl hdl)
 {
+  // Prepare and send a message to subscribe to trade updates.
   //std::string msg = "hello";
-  std::string msg = "event:subscribe,channel:trades,chanId:19111,symbol:tBTCUSD,pair:BTCUSD";
-  std::cout << "on_open: send " << msg << std::endl;
+  std::string msg = "{event:subscribe,channel:trades,chanId:19111,symbol:tBTCUSD,pair:BTCUSD}";
+  std::cout << "send ↑: " << msg << std::endl;
   client->send(hdl, msg, websocketpp::frame::opcode::text);
 }
 
-
+// Initialize the SSL/TLS context for secure communication.
 websocketpp::lib::shared_ptr<SslContext> on_tls_init()
 {
   auto ctx = websocketpp::lib::make_shared<SslContext>(boost::asio::ssl::context::sslv23);
   return ctx;
 }
  
-
+// Disables logging (protocol) to reduce console output.
 void turn_off_logging(Client& client)
 {
   client.clear_access_channels(websocketpp::log::alevel::all);
   client.clear_error_channels(websocketpp::log::elevel::all);
 }
 
-
+// Handling incoming WebSocket messages.
 void set_message_handler(Client& client)
 {
   client.set_message_handler(websocketpp::lib::bind(&on_message, &client, ::_1, ::_2));
@@ -54,13 +55,13 @@ void set_open_handler(Client& client)
   client.set_open_handler(websocketpp::lib::bind(&on_open, &client, ::_1));
 }
 
-
+// Initialize the SSL/TLS context for secure WebSocket communication.
 void set_tls_init_handler(Client& client)
 {
   client.set_tls_init_handler(websocketpp::lib::bind(&on_tls_init));
 }
 
-
+// Set the WebSocket server URL and establishes the WebSocket connection.
 void set_url(Client& client, std::string url)
 {
   websocketpp::lib::error_code ec;
@@ -74,22 +75,28 @@ void set_url(Client& client, std::string url)
 
 int main()
 {
+  // Initialize the WebSocket client
   Client client;
 
-  // Comment out to show access information and errors.
-  turn_off_logging(client);
+  // Disable logging messages
+  //turn_off_logging(client);
 
+  // Initialize the Asio event loop
   client.init_asio();
 
+  // Set the TLS initialization handler
   set_tls_init_handler(client);
 
+  // Set the connection open handler
   set_open_handler(client);
 
+  // Set the message handler
   set_message_handler(client);
 
+  // Set the WebSocket server URL and establish the connection
   set_url(client, "wss://api-pub.bitfinex.com/ws/2");
 
-
+  // Start the WebSocket client event loop
   client.run();
 
   return 0;
