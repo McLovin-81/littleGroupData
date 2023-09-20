7a6407e90713ff1090fc8108bf4d2dfc3cb5ccd0
Felipe <cristian.castillo.barrero@deutsche-boerse.com>
2023-09-20 16:54:25 +0200

    refactoring

--- webSocket/webSocketClient.cpp
+++ webSocket/webSocketClient.cpp
@@ -1,19 +1,57 @@
 #include <websocketpp/client.hpp>
 #include <websocketpp/config/asio_client.hpp>
-#include <websocketpp/transport/asio/connection.hpp> // Proxy function
+#include <websocketpp/transport/asio/connection.hpp> // -> Proxy function
 
-// Define websocket client type
-using Client = websocketpp::client<websocketpp::config::asio_tls_client>;
 
-using ConnectionHdl = websocketpp::connection_hdl;
-
-using SslContext = websocketpp::lib::asio::ssl::context;
+// Define various types
+typedef websocketpp::client<websocketpp::config::asio_tls_client> Client;
+typedef websocketpp::connection_hdl ConnectionHdl;
+typedef websocketpp::lib::asio::ssl::context SslContext;
 
 using websocketpp::lib::placeholders::_1;
-
 using websocketpp::lib::placeholders::_2;
 
+
+void on_message(Client* client, ConnectionHdl hdl, websocketpp::config::asio_client::message_type::ptr msg);
+void on_open(Client* client, ConnectionHdl hdl);
+void turn_off_logging(Client& client);
+void set_message_handler(Client& client);
+void set_open_handler(Client& client);
+void set_tls_init_handler(Client& client);
+void set_url(Client& client, std::string url);
+
+
  
+int main()
+{
+  // Initialize the WebSocket client
+  Client client;
+
+  // Disable logging messages
+  turn_off_logging(client);
+
+  // Initialize the Asio event loop
+  client.init_asio();
+
+  // Set the TLS initialization handler
+  set_tls_init_handler(client);
+
+  // Set the connection open handler
+  set_open_handler(client);
+
+  // Set the message handler
+  set_message_handler(client);
+
+  // Set the WebSocket server URL and establish the connection
+  set_url(client, "wss://api-pub.bitfinex.com/ws/2");
+
+  // Start the WebSocket client event loop
+  client.run();
+
+  return 0;
+}
+
+
 // Callback function when a message is received.
 void on_message(Client* client, ConnectionHdl hdl, websocketpp::config::asio_client::message_type::ptr msg)
 {
@@ -33,8 +71,8 @@ void on_open(Client* client, ConnectionHdl hdl)
 // Initialize the SSL/TLS context for secure communication.
 websocketpp::lib::shared_ptr<SslContext> on_tls_init()
 {
-  auto ctx = websocketpp::lib::make_shared<SslContext>(boost::asio::ssl::context::sslv23);
-  return ctx;
+  auto sslContext_ptr = websocketpp::lib::make_shared<SslContext>(boost::asio::ssl::context::sslv23);
+  return sslContext_ptr;
 }
  
 // Disables logging (protocol) to reduce console output.
@@ -73,35 +111,4 @@ void set_url(Client& client, std::string url)
   connection->set_proxy("http://squid-proxy.gcp.dbgcloud.io:3128", ec);
 
   client.connect(connection);
-}
-
- 
-
-int main()
-{
-  // Initialize the WebSocket client
-  Client client;
-
-  // Disable logging messages
-  turn_off_logging(client);
-
-  // Initialize the Asio event loop
-  client.init_asio();
-
-  // Set the TLS initialization handler
-  set_tls_init_handler(client);
-
-  // Set the connection open handler
-  set_open_handler(client);
-
-  // Set the message handler
-  set_message_handler(client);
-
-  // Set the WebSocket server URL and establish the connection
-  set_url(client, "wss://api-pub.bitfinex.com/ws/2");
-
-  // Start the WebSocket client event loop
-  client.run();
-
-  return 0;
 }
