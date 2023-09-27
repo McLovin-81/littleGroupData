f1c7cae93caf39e75534ece10377228a1ccd9e8b
Felipe <cristian.castillo.barrero@deutsche-boerse.com>
2023-09-27 14:13:52 +0200

    Commets deleted

--- webSocket/configuration/config.h
+++ webSocket/configuration/config.h
@@ -21,24 +21,3 @@ struct Config
 };
 
 #endif // CONFIG_H
-
-
-
-/*
-INCLUDE GUARDS ensure that compiler will process this file only once,
-no matter how many times it is INCLUDED.
-Included guards are just series of preprocessor directives that
-guarantees file will only be included one.
-
-Preprocessors used:
-
-    #ifndef:    If not defined, determines if provided macros does not exist.
-    #define:    Defines the macros.
-    
-    ...
-    
-    #endif:     Close off #ifndef directive.
-
-The block of statements between #ifndef and #endif will be
-executed only if the macro or the identifier with #ifndef is NOT
-defined.*/
